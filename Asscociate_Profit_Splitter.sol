eef336cd6b17884f61e091cb597bab5bf1841de3
Youssef Abdelwahab <youssefabdelwahab10@gmail.com>
2021-05-15 19:10:26 -0400

    Update Asscociate_Profit_Splitter.sol

--- Asscociate_Profit_Splitter.sol
+++ Asscociate_Profit_Splitter.sol
@@ -1,14 +1,12 @@
 pragma solidity ^0.5.0;
 
-// lvl 1: equal split
 contract AssociateProfitSplitter {
-    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
     address payable HR = msg.sender;
     address payable employee_one; 
     address payable employee_two; 
     address payable employee_three;
     
-    mapping (address => uint) balances; 
+   
 
 
     constructor(address payable _one, address payable _two, address payable _three) public {
@@ -22,34 +20,23 @@ contract AssociateProfitSplitter {
     }
 
     function deposit() public payable {
-        // @TODO: Split `msg.value` into three
         uint amount = msg.value / 3;
-
-        // @TODO: Transfer the amount to each employee
-        // Your code here!
+        
         employee_one.transfer(amount);
         employee_two.transfer(amount); 
         employee_three.transfer(amount);
 
-        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
-    
+       
         msg.sender.transfer(msg.value - (amount *3 ));
     }
 
     function() external payable {
-        // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
         uint amount = msg.value / 3;
-
-        // @TODO: Transfer the amount to each employee
-        // Your code here!
+        
         employee_one.transfer(amount);
         employee_two.transfer(amount); 
         employee_three.transfer(amount);
-
-        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        
         msg.sender.transfer(msg.value - (amount *3 ));
     }
 }
