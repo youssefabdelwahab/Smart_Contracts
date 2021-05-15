92d1fd539d213fc7ae5a30816817f1bd494a75f8
Youssef Abdelwahab <youssefabdelwahab10@gmail.com>
2021-05-15 19:11:23 -0400

    Update Tiered_Profit_Splitter.sol

--- Tiered_Profit_Splitter.sol
+++ Tiered_Profit_Splitter.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.5.0;
 
-// lvl 1: equal split
+
 contract AssociateProfitSplitter {
-    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
     address payable HR = msg.sender;
     address payable employee_one; 
     address payable employee_two; 
     address payable employee_three;
     
-    mapping (address => uint) balances; 
+    
 
 
     constructor(address payable _one, address payable _two, address payable _three) public {
@@ -22,13 +21,10 @@ contract AssociateProfitSplitter {
     }
 
     function deposit() public payable {
-        // @TODO: Split `msg.value` into three
         uint points = msg.value /100;
         uint amount = points * 60;
         uint total = amount; 
 
-        // @TODO: Transfer the amount to each employee
-        // Your code here!
         employee_one.transfer(amount);
         amount = points * 25;
         total += amount;
@@ -39,22 +35,16 @@ contract AssociateProfitSplitter {
  
         
 
-        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
         uint remainder = (msg.value - total);
     
         employee_one.transfer(remainder);
     }
 
     function() external payable {
-        // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
         uint points = msg.value / 100;
         uint amount = points * 60;
         uint total = amount; 
 
-        // @TODO: Transfer the amount to each employee
-        // Your code here!
         employee_one.transfer(amount);
         amount = points * 25;
         total += amount;
@@ -62,9 +52,7 @@ contract AssociateProfitSplitter {
         amount = points * 15;
         total += amount;
         employee_three.transfer(amount);
-
-        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        
         uint remainder = (msg.value - total);
         employee_one.transfer(remainder);
     }
